37fb598300f0fd78cc3efdec8c407fedd2086d81
Nathan Rosenboom <nathan.rosenboom@gmail.com>
2020-07-23 15:04:56 +0800

    Created seeder for user data

--- database/seeds/userSeeder.js
+++ database/seeds/userSeeder.js
@@ -2,14 +2,21 @@ const faker = require("faker");
 const User = require("../../models/User");
 
 function seedData() {
-    User.collection.deleteMany();
+  User.collection.deleteMany();
 
-const user = new User({
-    name: faker.name.findName(),
-    email: faker.internet.email(),
-    password: "secret",
-})
-user.save()
-};
+  for (let index = 0; index < 10; index++) {
+
+    const user = new User({
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      password: "secret",
+
+    });
+
+    user.save();
+
+  }
+
+}
 
 module.exports = seedData;
