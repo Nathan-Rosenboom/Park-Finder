8808cc1abf05cb991de63aa703d08da9899e1a20
Nathan Rosenboom <nathan.rosenboom@gmail.com>
2020-07-23 14:18:43 +0800

    Moved mongodb connection from server to separate configuration file

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 3001;
+
+const routes = require("./routes");
 const app = express();
+const connectDb = require("./config/database");
+
+const PORT = process.env.PORT || 3001;
+
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -10,14 +14,15 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-
+connectDb();
 // Define API routes here
-
+app.use(routes);
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
