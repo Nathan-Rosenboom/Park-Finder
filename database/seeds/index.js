1827ad3fc9b061c7b083806d55f5644d57192166
Nathan Rosenboom <nathan.rosenboom@gmail.com>
2020-07-24 08:25:43 +0800

    Added comment seeder to seeder index

--- database/seeds/index.js
+++ database/seeds/index.js
@@ -1,11 +1,13 @@
 const connectDb = require('../../config/database');
 const userSeeder = require('./userSeeder');
 const parkSeeder = require("./parkSeeder");
+const commentSeeder = require("./commentSeeder");
 connectDb();
 
 async function seed(){
     await userSeeder();
     await parkSeeder();
+    await commentSeeder();
 };
 
 seed();
