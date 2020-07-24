384aaa8a244ca65bdb880ea1877666764f0e8e0f
Nathan Rosenboom <nathan.rosenboom@gmail.com>
2020-07-24 14:25:10 +0800

    Created login form

---
+++ client/src/containers/home/LoginForm.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import Form  from "react-bootstrap/Form";
+import Button  from "react-bootstrap/Button";
+
+import "./Login.css";
+
+function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <div className="Login">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="email" bssize="large">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            autoFocus
+            type="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group controlId="password" bssize="large">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            type="password"
+          />
+        </Form.Group>
+        <Button block bssize="large" disabled={!validateForm()} type="submit">
+          Login
+        </Button>
+      </Form>
+    </div>
+  );
+}
+export default LoginForm;
