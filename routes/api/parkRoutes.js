44f36a60c729a4b096abe11c999a961555067dcf
Nathan Rosenboom <nathan.rosenboom@gmail.com>
2020-07-24 11:57:41 +0800

    Created api routes for user model

---
+++ routes/api/parkRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("users/", (req, res) => {
+
+});
+
+router.get("users/:id", (req, res) => {
+
+});
+
+module.exports = router;
