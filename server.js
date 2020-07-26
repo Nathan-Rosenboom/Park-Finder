d65e6def4ea4f122d2772a342025903ae2cf02c1
Nathan Rosenboom <nathan.rosenboom@gmail.com>
2020-07-27 03:38:05 +0800

    Imported packages required for user authentication

--- server.js
+++ server.js
@@ -2,23 +2,29 @@ const express = require("express");
 const session = require("express-session");
 const passport = require("passport");
 const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const corsConfig = require("./config/cors");
 const MongoStore = require("connect-mongo")(session);
 dotenv.config({ path: ".env" });
 const routes = require("./routes");
-const app = express();
 const connectDb = require("./config/database");
+
+const app = express();
+
+connectDb();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.SESSION_SECRET));
+app.use(cors(corsConfig));
 
 const PORT = process.env.PORT || 3001;
 
-
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-connectDb();
 
 app.use(
   session({
@@ -36,13 +42,7 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 // Define API routes here
-app.use(routes);
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
-
+app.use("/api", routes);
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
