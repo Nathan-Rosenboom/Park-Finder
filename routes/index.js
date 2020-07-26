57c764233781f9050d6b01bd838bdd0fe6e542a5
Nathan Rosenboom <nathan.rosenboom@gmail.com>
2020-07-27 00:00:19 +0800

    Added authentication routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const auth = require("./api/auth/auth");
+const user = require("./api/userRoutes");
+const park = require("./api/parkRoutes");
+const comment = require("./api/comment Routes");
+const isAuthenticated = require("../middleware/authenticationMiddleware");
+
 
 const router = express.Router()
 
-
+router.use(auth);
+router.use(isAuthenticated);
+router.use(user);
+router.use(park);
+router.use(comment);
 
 module.exports = router;
