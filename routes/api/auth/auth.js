698eee6e548b73759f73f46a0f475a00efc7070a
Nathan Rosenboom <nathan.rosenboom@gmail.com>
2020-07-27 00:01:51 +0800

    Created authentication routes

---
+++ routes/api/auth/auth.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const router = express.router;
+const passport = require("passport");
+
+router.post("login", (req, res) => {
+    passport.authenticate("local", (err, user, info) => {
+        if (err) {
+            return next(err);
+        }
+        if (!user) {
+            return res.status(422).json({
+                data: info.msg,
+            });
+        }
+        req.logIn(user, (err) => {
+            if (err) {
+                return next(err);
+            }
+            req.json({
+                data: user,
+            });
+        });
+    })(req, res, next);
+});
+
+router.post("logout", (req, res) => {
+    req.logout();
+    req.session.destroy((err) => {
+        if (err) {
+            res.json({
+                data: {
+                    error: "Failed to destroy the session during logout.",
+                    err,
+                },
+            });
+        }
+        req.user = null;
+        res.json({
+            data: {
+                msg: "Success",
+            },
+        });
+    });
+});
+
+module.exports = router;
