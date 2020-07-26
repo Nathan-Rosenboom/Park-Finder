bc2a4a00481fe1d6c376fae6315643089ae55a4a
Nathan Rosenboom <nathan.rosenboom@gmail.com>
2020-07-27 03:35:37 +0800

    Removed login form validation for testing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,21 +2,23 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import GlobalStore from "./utils/GlobalStore";
 
 import Home from "./pages/Home";
+//import Register from "./pages/Register";
+import Parks from "./pages/CreatePark";
 
 function App() {
   return (
     <Router>
-      <div>
+      <GlobalStore.GlobalProvider>
         <Switch>
           <Route exact path="/" component={Home} />
-          {/*<Route exact path="/" component={Home} />
-          <Route exact path="/" component={Home} />*/}
+          {/*<Route exact path="/register" component={Register} />*/}
+        <Route exact path="/parks" component={Parks} />
         </Switch>
-     </div>
+      </GlobalStore.GlobalProvider>
     </Router>
-
   );
 }
 
--- client/src/containers/home/LoginForm.jsx
+++ client/src/containers/home/LoginForm.jsx
@@ -1,20 +1,63 @@
 import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 import Form  from "react-bootstrap/Form";
 import Button  from "react-bootstrap/Button";
+import axios from "axios";
 
 import "./Login.css";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const history = useHistory();
 
-  function validateForm() {
-    return email.length > 0 && password.length > 0;
-  }
+  const [errors, setErrors] = useState([]);
 
-  function handleSubmit(event) {
-    event.preventDefault();
-  }
+  const [payload, setPayload] = useState({});
+
+  const handleChange = async (event) => {
+      const type = event.target.name;
+
+      // payload looks like: {
+      //     email: '',
+      //     password: '',
+      // }
+      setPayload({
+          ...payload,
+          [type]: event.target.value, // dynamically set the type of payload
+      });
+  };
+
+  const handleSubmit = async (event) => {
+      event.preventDefault();
+      // call api to login
+
+      const response = await axios
+          .post(
+              "http://localhost:3001/api/login",
+              {
+                  email: payload.email,
+                  password: payload.password,
+              },
+              {
+                  withCredentials: true
+              }
+          )
+          .then((response) => {
+              history.push("/parks");
+          })
+          .catch((err) => {
+              // not authenticated
+              console.log(err.response);
+              if (err.response.data.errors) {
+                  const errorMsg = err.response.data.errors.map(
+                      (err) => err.msg
+                  );
+                  // failed to register
+                  setErrors([...errorMsg]);
+              } else {
+                  setErrors(['Whoops please enter your credentials']);
+              }
+          });
+  };
 
   return (
     <div className="Login">
@@ -23,20 +66,22 @@ function LoginForm() {
           <Form.Label>Email</Form.Label>
           <Form.Control
             autoFocus
+            name="email"
             type="email"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
+            label="Email"
+            onChange={handleChange}
           />
         </Form.Group>
         <Form.Group controlId="password" bssize="large">
           <Form.Label>Password</Form.Label>
           <Form.Control
-            value={password}
-            onChange={e => setPassword(e.target.value)}
+            name="password"
+            label="Password"
+            onChange={handleChange}
             type="password"
           />
         </Form.Group>
-        <Button block bssize="large" disabled={!validateForm()} type="submit">
+        <Button block bssize="large" type="submit">
           Login
         </Button>
       </Form>
