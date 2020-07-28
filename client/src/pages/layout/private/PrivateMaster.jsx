ec574acfd470630954d79e97de953d78a7266fde
Nathan Rosenboom <nathan.rosenboom@gmail.com>
2020-07-28 13:27:41 +0800

    Added styling to PrivateMaster

--- client/src/pages/layout/private/PrivateMaster.jsx
+++ client/src/pages/layout/private/PrivateMaster.jsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navbar from "./Navbar";
+import GlobalStore from "../../../utils/GlobalStore"
+import { useHistory } from "react-router-dom";
+import axios from 'axios'
 
-function PublicMaster() {
+function PublicMaster(props) {
+    const store = GlobalStore.useGlobalContext()
+    const history = useHistory()
+
+    useEffect(() => {
+        axios.get('http://localhost:3001/api/current-user', {
+            withCredentials: true,
+        })
+            .then((response) => {
+                console.log(response)
+                store.auth.dispatchAuth({
+                    type: 'set-user',
+                    payload: response.data.data
+                })
+            }).catch((err) => {
+                if(err.response.status === 401){
+                    return history.push('/')
+                }
+                console.log({err});
+            })
+    }, [])
     return (
-
+        <main {...props}>
             <Navbar />
+        {props.children}
+        </main>
 
     )
 }
