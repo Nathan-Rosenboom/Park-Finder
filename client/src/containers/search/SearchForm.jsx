f48e8155ee2496dd33396404e15bd2dab213a1d8
Nathan Rosenboom <nathan.rosenboom@gmail.com>
2020-07-29 13:05:50 +0800

    Added styling to SearchForm

--- client/src/containers/search/SearchForm.jsx
+++ client/src/containers/search/SearchForm.jsx
@@ -8,7 +8,7 @@ import "./SearchForm.css";
 
 function SearchForm() {
   return (
-    <Card>
+    <Card className="formCard">
       <Card.Title>Search</Card.Title>
       <Card.Body>
         <Form>
@@ -18,38 +18,36 @@ function SearchForm() {
               <option>Name</option>
               <option>Address</option>
             </Form.Control>
-            <Form.Control type="text" placeholder="" />
+            <Form.Control className="formInput" type="text" placeholder="" />
           </Form.Group>
           <Form.Label>Filter Results</Form.Label>
-          {["checkbox"].map((type) => (
-            <div key={`inline-${type}`} className="mb-3">
+            <div key="inline-checkbox" className="mb-3">
               <Form.Check
                 inline
                 label="Playground"
-                type={type}
-                id={`inline-${type}-1`}
+                type="checkbox"
+                id="inline-checkbox-1"
               />
               <Form.Check
                 inline
                 label="Pets Allowed"
-                type={type}
-                id={`inline-${type}-2`}
+                type="checkbox"
+                id="inline-checkbox-2"
               />
               <Form.Check
                 inline
                 label="Exercise Facilities"
-                type={type}
-                id={`inline-${type}-3`}
+                type="checkbox"
+                id="inline-checkbox-3"
               />
               <Form.Check
                 inline
                 label="Toilets"
-                type={type}
-                id={`inline-${type}-4`}
+                type="checkbox"
+                id="inline-checkbox-4"
               />
             </div>
-          ))}
-          <Button variant="primary" type="submit">
+          <Button variant="secondary" type="submit">
             Search
           </Button>
         </Form>
