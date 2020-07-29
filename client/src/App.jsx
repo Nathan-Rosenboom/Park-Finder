fab9e4958fbc12f92bfd3e53b6318a2c2b3b8df0
Nathan Rosenboom <nathan.rosenboom@gmail.com>
2020-07-29 15:16:00 +0800

    Added register page to router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "react-bootstrap";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import GlobalStore from "./utils/GlobalStore";
 import "./App.css";
 
 import Home from "./pages/Home";
-//import Register from "./pages/Register";
 import Parks from "./pages/Parks";
+import Create from "./pages/Create";
+import Register from "./pages/Register";
 
 function App() {
   return (
@@ -15,13 +16,13 @@ function App() {
       <GlobalStore.GlobalProvider>
         <Switch>
           <Route exact path="/" component={Home} />
-          {/*<Route exact path="/register" component={Register} />*/}
-        <Route exact path="/parks" component={Parks} />
-        </Switch>
+          <Route exact path="/create" component={Create} />
+          <Route exact path="/parks" component={Parks} />
+          <Route exact path="/register" component={Register} />
+        </Switch> 
       </GlobalStore.GlobalProvider>
     </Router>
   );
 }
-
 
 export default App;
