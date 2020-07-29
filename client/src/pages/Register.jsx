2eb16d0a2d36b91220ae71dca86ef3a7088fdd95
Nathan Rosenboom <nathan.rosenboom@gmail.com>
2020-07-29 15:24:06 +0800

    Created Register page

---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import PublicMaster from "./layout/public/PublicMaster";
+import RegisterForm from "../containers/home/RegisterForm";
+
+function Register (props) {
+
+    return (
+        <main>
+        <PublicMaster/>
+        <RegisterForm/>
+        </main>
+    )
+}
+
+export default Register;
