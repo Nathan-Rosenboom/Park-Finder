b884f96c1bf2cfb373006e9d73e13357ca032dc7
Nathan Rosenboom <nathan.rosenboom@gmail.com>
2020-07-29 19:55:21 +0800

    Added logout button

--- client/src/pages/layout/private/Navbar.jsx
+++ client/src/pages/layout/private/Navbar.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import axios from "axios";
 
 function Navigation(props) {
+
+  const logout = () => {
+    axios.get('http://localhost:3001/api/logout')
+        .then((response) => {
+            window.location.href = '/'
+        }).catch((err) => {
+            console.log(err);
+        })
+}
     return (
     <Navbar bg="dark" variant="dark">
     <Navbar.Brand href="#home">Park Finder</Navbar.Brand>
     <Nav className="mr-auto">
       <Nav.Link href="/parks">Search</Nav.Link>
       <Nav.Link href="/create">Add Park</Nav.Link>
+      <Nav.Link onClick={logout}>Logout</Nav.Link>
     </Nav>
   </Navbar>
     )
