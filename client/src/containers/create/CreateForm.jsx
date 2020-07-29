72427618a4c435d0a4799f285d3f84efca83aeef
Nathan Rosenboom <nathan.rosenboom@gmail.com>
2020-07-29 19:49:18 +0800

    Added bootstrap elements

--- client/src/containers/create/CreateForm.jsx
+++ client/src/containers/create/CreateForm.jsx
@@ -6,49 +6,127 @@ import Form from "react-bootstrap/Form";
 import axios from "axios";
 import "./CreateForm.css";
 
-function CreateForm() {
+function CreateForm(props) {
+
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+    const [streetAddress, setStreetAddress] = useState("");
+    const [city, setCity] = useState("");
+    const [state, setState] = useState("");
+    const [playground, setPlayground] = useState("");
+    const [toilets, setToilets] = useState("");
+    const [exerciseFacilities, setExerciseFacilities] = useState("");
+    const [petsAllowed, setPetsAllowed] = useState("");
+
+    const handleParkName = (event) => {
+        setName(event.target.value)
+    }
+
+    const handleParkDescription = (event) => {
+        setDescription(event.target.value)
+    }
+
+    const handleParkStreetAddress = (event) => {
+        setStreetAddress(event.target.value)
+    }
+
+    const handleParkCity = (event) => {
+        setCity(event.target.value)
+    }
+
+    const handleParkState = (event) => {
+        setState(event.target.value)
+    }
+
+    const handleParkPlayground = (event) => {
+        setPlayground(event.target.value)
+    }
+
+    const handleParkToilets = (event) => {
+        setToilets(event.target.value)
+    }
+
+    const handleParkExerciseFacilities = (event) => {
+        setExerciseFacilities(event.target.value)
+    }
+
+    const handleParkPetsAllowed = (event) => {
+        setPetsAllowed(event.target.value)
+    }
+
+    const createPark = (event) => {
+        event.preventDefault();
+        axios.post('http://localhost:3001/api/parks', {
+            name,
+            description,
+            streetAddress,
+            city,
+            state,
+            playground,
+            toilets,
+            exerciseFacilities,
+            petsAllowed
+        }).then((response) => {
+            const newPark = response.data.data;
+            props.setParks([
+                newPark,
+                ...props.parks,
+            ])
+        });
+    }
   return (
     <Card className="formCard">
       <Card.Title>Add a Park</Card.Title>
       <Card.Body>
-        <Form>
-          <Form.Group controlId="SearchForm">
-            <Form.Label>Search for park by:</Form.Label>
-            <Form.Control as="select">
-              <option>Name</option>
-              <option>Address</option>
-            </Form.Control>
-            <Form.Control className="formInput" type="text" placeholder="" />
+        <Form onSubmit={createPark}>
+          <Form.Group controlId="CreateForm">
+            <Form.Control onChange={handleParkName} className="formInput" type="text" placeholder="Park Name" />
           </Form.Group>
-          <Form.Label>Filter Results</Form.Label>
+          <Form.Group controlId="CreateForm">
+            <Form.Control onChange={handleParkDescription} className="formInput" type="text" placeholder="Description" />
+          </Form.Group>
+          <Form.Group controlId="CreateForm">
+            <Form.Control onChange={handleParkStreetAddress} className="formInput" type="text" placeholder="Street Address" />
+          </Form.Group>
+          <Form.Group controlId="CreateForm">
+            <Form.Control onChange={handleParkCity} className="formInput" type="text" placeholder="City" />
+          </Form.Group>
+          <Form.Group controlId="CreateForm">
+            <Form.Control onChange={handleParkState} className="formInput" type="text" placeholder="State" />
+          </Form.Group>
+          <Form.Label>Tags</Form.Label>
             <div key="inline-checkbox" className="mb-3">
               <Form.Check
                 inline
                 label="Playground"
                 type="checkbox"
                 id="inline-checkbox-1"
+                onChange={handleParkPlayground}
               />
               <Form.Check
                 inline
                 label="Pets Allowed"
                 type="checkbox"
                 id="inline-checkbox-2"
+                onChange={handleParkPetsAllowed}
               />
               <Form.Check
                 inline
                 label="Exercise Facilities"
                 type="checkbox"
                 id="inline-checkbox-3"
+                onChange={handleParkExerciseFacilities}
               />
               <Form.Check
                 inline
                 label="Toilets"
                 type="checkbox"
                 id="inline-checkbox-4"
+                onChange={handleParkToilets}
               />
             </div>
           <Button variant="secondary" type="submit">
-            Search
+            Submit
           </Button>
         </Form>
       </Card.Body>
