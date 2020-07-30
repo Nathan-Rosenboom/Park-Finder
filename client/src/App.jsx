7b67e00c7ce739f3972dd60e948cf99f7951bde5
Nathan Rosenboom <nathan.rosenboom@gmail.com>
2020-07-30 14:58:24 +0800

    Removed cors

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import GlobalStore from "./utils/GlobalStore";
+import GlobalStore from "./utils/GlobalStore.jsx";
 import "./App.css";
 
-import Home from "./pages/Home";
-import Parks from "./pages/Parks";
-import Create from "./pages/Create";
-import Register from "./pages/Register";
+import Home from "./pages/Home.jsx";
+import Parks from "./pages/Parks.jsx";
+import Create from "./pages/Create.jsx";
+import Register from "./pages/Register.jsx";
+import Park from "./pages/Park.jsx";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           <Route exact path="/create" component={Create} />
           <Route exact path="/parks" component={Parks} />
           <Route exact path="/register" component={Register} />
+          <Route exact path="/park" component={Park} />
         </Switch> 
       </GlobalStore.GlobalProvider>
     </Router>
