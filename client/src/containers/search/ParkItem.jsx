6a350f006ee7f21aca02b89aed1b319f959d8a0f
Nathan Rosenboom <nathan.rosenboom@gmail.com>
2020-07-30 15:03:26 +0800

    Added link to individual park page

--- client/src/containers/search/ParkItem.jsx
+++ client/src/containers/search/ParkItem.jsx
@@ -6,41 +6,20 @@ import axios from "axios";
 import "./ParkItem.css";
 function ParkItem(props) {
     const history = useHistory();
-    const [comments, setComments] = useState(null);
-
-
-    const addComment = (comment) => {
-        setComments([
-            ...comments,
-            comment
-        ])
-    };
-
-
-    const toggleComments = (hide = false) => {
-        if (hide) {
-            return setComments(null);
-        }
-        axios
-            .get(`http://localhost:3001/api/comments?post_id=${props.park._id}`)
-            .then((response) => {
-                setComments(response.data.data);
-            });
-    };
 
     const handleClick = async (event) => {
         event.preventDefault();
-  
-        const response = await axios
+        const id = event.target.value
+        await axios
             .get(
-                `http://localhost:3001/api/parks?park_id=${props.park._id}`,
+                `/api/parks?park_id=${id}`,
                 {
                     withCredentials: true
                 }
             )
             .then((response) => {
-                history.push(`/parks/:${response._id}`);
-                console.log(response)
+                history.push(`/park/${id}`);
+                console.log(id)
             })
             .catch((err) => {
                 console.log(err);
@@ -55,7 +34,7 @@ function ParkItem(props) {
             <Card.Text>
                 {props.park.description}
             </Card.Text>
-            <Button bsPrefix="viewButton" onClick={handleClick} variant="warning">View Details</Button>
+            <Button bsPrefix="viewButton" value={props.park._id} onClick={handleClick} variant="warning">View Details</Button>
             </Card>
         </div>
 
