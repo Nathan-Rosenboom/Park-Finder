0c4b9b3540f2714790bb52bbeeb890a6b7023874
Nathan Rosenboom <nathan.rosenboom@gmail.com>
2020-07-31 22:00:24 +0800

    Enabled cors access

--- server.js
+++ server.js
@@ -20,6 +20,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.SESSION_SECRET));
 //app.use(cors(corsConfig));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 const PORT = process.env.PORT || 3001;
 
 if (process.env.NODE_ENV === "production") {
