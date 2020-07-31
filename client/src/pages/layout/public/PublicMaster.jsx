4759a66da764d46517a7a9d8ae53e76ef52f11f9
Nathan Rosenboom <nathan.rosenboom@gmail.com>
2020-08-01 01:03:20 +0800

    Added footer

--- client/src/pages/layout/public/PublicMaster.jsx
+++ client/src/pages/layout/public/PublicMaster.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 function PublicMaster() {
     return (
-
+        <>
             <Navbar />
 
+            <Footer />
+        </>
     )
 }
 
