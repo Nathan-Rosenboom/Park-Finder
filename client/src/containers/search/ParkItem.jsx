fbe056c7ad78211a5a9885815c28270fe7575e53
Nathan Rosenboom <nathan.rosenboom@gmail.com>
2020-07-31 22:39:32 +0800

    Changed content of ParkItem

--- client/src/containers/search/ParkItem.jsx
+++ client/src/containers/search/ParkItem.jsx
@@ -1,12 +1,50 @@
 import React from 'react';
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 import ParkModal from "./ParkModal";
 import "./ParkItem.css";
 
 function ParkItem(props) {
     const [modalShow, setModalShow] = React.useState(false);
+    let toilets = props.park.toilets.toString();
+  let playground = props.park.playground.toString();
+  let exerciseFacilities = props.park.exerciseFacilities.toString();
+  let petsAllowed = props.park.petsAllowed.toString();
+  let colourClass = "red";
+  let toiletIcon = "";
+  if (toilets === "true") {
+    toiletIcon = "far fa-check-circle";
+    colourClass = "green"
+  } else {
+    toiletIcon = "far fa-times-circle";
+  }
+
+  let playgroundIcon = "";
+  if (playground === "true") {
+    playgroundIcon = "far fa-check-circle";
+    colourClass = "green"
+  } else {
+    playgroundIcon = "far fa-times-circle";
+  }
+
+  let petsAllowedIcon = "";
+  if (petsAllowed === "true") {
+    petsAllowedIcon = "far fa-check-circle";
+    colourClass = "green"
+  } else {
+    petsAllowedIcon = "far fa-times-circle";
+  }
+
+  let exerciseFacilitiesIcon = "";
+  if (exerciseFacilities === "true") {
+    exerciseFacilitiesIcon = "far fa-check-circle";
+    colourClass = "green"
+  } else {
+    exerciseFacilitiesIcon = "far fa-times-circle";
+  }
     
     return (
 
@@ -14,7 +52,7 @@ function ParkItem(props) {
             <Card className="resultCard" bg="secondary">
             <Card.Title>{props.park.name}</Card.Title>
             <Card.Text>
-                {props.park.description}
+                <p>{props.park.streetAddress}, {props.park.city}, {props.park.state}</p>
             </Card.Text>
             <Button bsPrefix="viewButton" value={props.park._id} onClick={() => setModalShow(true)} variant="warning">View Details</Button>
             <ParkModal  
